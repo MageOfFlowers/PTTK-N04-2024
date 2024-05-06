9b6f41fa68d3b14dc0c8bc80b0bd0c3507d0bfca
MageOfFlowers <100255997+MageOfFlowers@users.noreply.github.com>
2024-05-06 11:34:32 +0700

    aaaaaaaaaa

---
+++ PTTK/MHXacThucHoSoDaXuLy.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace PTTK
+{
+    partial class MHXacThucHoSoDaXuLy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbHoTen = new System.Windows.Forms.Label();
+            this.lbNgayNopHoSo = new System.Windows.Forms.Label();
+            this.lbDiaChi = new System.Windows.Forms.Label();
+            this.lbSDT = new System.Windows.Forms.Label();
+            this.lbCCCD = new System.Windows.Forms.Label();
+            this.BangCapDGV = new System.Windows.Forms.DataGridView();
+            this.UngVienPB = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.BangCapDGV)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UngVienPB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.8F);
+            this.label1.Location = new System.Drawing.Point(268, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(268, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thông tin Ứng viên";
+            // 
+            // lbHoTen
+            // 
+            this.lbHoTen.AutoSize = true;
+            this.lbHoTen.Location = new System.Drawing.Point(215, 93);
+            this.lbHoTen.Name = "lbHoTen";
+            this.lbHoTen.Size = new System.Drawing.Size(46, 16);
+            this.lbHoTen.TabIndex = 1;
+            this.lbHoTen.Text = "Họ tên";
+            // 
+            // lbNgayNopHoSo
+            // 
+            this.lbNgayNopHoSo.AutoSize = true;
+            this.lbNgayNopHoSo.Location = new System.Drawing.Point(526, 93);
+            this.lbNgayNopHoSo.Name = "lbNgayNopHoSo";
+            this.lbNgayNopHoSo.Size = new System.Drawing.Size(105, 16);
+            this.lbNgayNopHoSo.TabIndex = 2;
+            this.lbNgayNopHoSo.Text = "Ngày nộp Hồ sơ";
+            // 
+            // lbDiaChi
+            // 
+            this.lbDiaChi.AutoSize = true;
+            this.lbDiaChi.Location = new System.Drawing.Point(215, 173);
+            this.lbDiaChi.Name = "lbDiaChi";
+            this.lbDiaChi.Size = new System.Drawing.Size(47, 16);
+            this.lbDiaChi.TabIndex = 3;
+            this.lbDiaChi.Text = "Địa chỉ";
+            // 
+            // lbSDT
+            // 
+            this.lbSDT.AutoSize = true;
+            this.lbSDT.Location = new System.Drawing.Point(215, 146);
+            this.lbSDT.Name = "lbSDT";
+            this.lbSDT.Size = new System.Drawing.Size(66, 16);
+            this.lbSDT.TabIndex = 4;
+            this.lbSDT.Text = "Điện thoại";
+            // 
+            // lbCCCD
+            // 
+            this.lbCCCD.AutoSize = true;
+            this.lbCCCD.Location = new System.Drawing.Point(215, 118);
+            this.lbCCCD.Name = "lbCCCD";
+            this.lbCCCD.Size = new System.Drawing.Size(64, 16);
+            this.lbCCCD.TabIndex = 5;
+            this.lbCCCD.Text = "Số CCCD";
+            // 
+            // BangCapDGV
+            // 
+            this.BangCapDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.BangCapDGV.Location = new System.Drawing.Point(65, 221);
+            this.BangCapDGV.Name = "BangCapDGV";
+            this.BangCapDGV.RowHeadersWidth = 51;
+            this.BangCapDGV.RowTemplate.Height = 24;
+            this.BangCapDGV.Size = new System.Drawing.Size(667, 173);
+            this.BangCapDGV.TabIndex = 6;
+            // 
+            // UngVienPB
+            // 
+            this.UngVienPB.Location = new System.Drawing.Point(80, 71);
+            this.UngVienPB.Name = "UngVienPB";
+            this.UngVienPB.Size = new System.Drawing.Size(105, 118);
+            this.UngVienPB.TabIndex = 7;
+            this.UngVienPB.TabStop = false;
+            // 
+            // MHXacThucHoSoDaXuLy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.UngVienPB);
+            this.Controls.Add(this.BangCapDGV);
+            this.Controls.Add(this.lbCCCD);
+            this.Controls.Add(this.lbSDT);
+            this.Controls.Add(this.lbDiaChi);
+            this.Controls.Add(this.lbNgayNopHoSo);
+            this.Controls.Add(this.lbHoTen);
+            this.Controls.Add(this.label1);
+            this.Name = "MHXacThucHoSoDaXuLy";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.BangCapDGV)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UngVienPB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbHoTen;
+        private System.Windows.Forms.Label lbNgayNopHoSo;
+        private System.Windows.Forms.Label lbDiaChi;
+        private System.Windows.Forms.Label lbSDT;
+        private System.Windows.Forms.Label lbCCCD;
+        private System.Windows.Forms.DataGridView BangCapDGV;
+        private System.Windows.Forms.PictureBox UngVienPB;
+    }
+}
---
+++ PTTK/MhChonNhaTuyenDung.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace PTTK
+{
+    partial class MhChonNhaTuyenDung
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.MaHSDGV = new System.Windows.Forms.DataGridView();
+            this.lbTenTuyenDung = new System.Windows.Forms.Label();
+            this.ChapNhanDGV = new System.Windows.Forms.DataGridView();
+            this.TuChoiDGV = new System.Windows.Forms.DataGridView();
+            this.lbChapNhan = new System.Windows.Forms.Label();
+            this.lbTuChoi = new System.Windows.Forms.Label();
+            this.lbSoLuong = new System.Windows.Forms.Label();
+            this.lbSoLuongYC = new System.Windows.Forms.Label();
+            this.lbSoLuongChapNhan = new System.Windows.Forms.Label();
+            this.lbSoLuongTuChoi = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.MaHSDGV)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ChapNhanDGV)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TuChoiDGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.8F);
+            this.label1.Location = new System.Drawing.Point(263, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(284, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Danh sách Ứng viên";
+            // 
+            // MaHSDGV
+            // 
+            this.MaHSDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MaHSDGV.Location = new System.Drawing.Point(51, 118);
+            this.MaHSDGV.Name = "MaHSDGV";
+            this.MaHSDGV.RowHeadersWidth = 51;
+            this.MaHSDGV.RowTemplate.Height = 24;
+            this.MaHSDGV.Size = new System.Drawing.Size(403, 338);
+            this.MaHSDGV.TabIndex = 1;
+            // 
+            // lbTenTuyenDung
+            // 
+            this.lbTenTuyenDung.AutoSize = true;
+            this.lbTenTuyenDung.Location = new System.Drawing.Point(51, 79);
+            this.lbTenTuyenDung.Name = "lbTenTuyenDung";
+            this.lbTenTuyenDung.Size = new System.Drawing.Size(157, 16);
+            this.lbTenTuyenDung.TabIndex = 2;
+            this.lbTenTuyenDung.Text = "Tên Yêu cầu Tuyển dụng";
+            // 
+            // ChapNhanDGV
+            // 
+            this.ChapNhanDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ChapNhanDGV.Location = new System.Drawing.Point(475, 143);
+            this.ChapNhanDGV.Name = "ChapNhanDGV";
+            this.ChapNhanDGV.RowHeadersWidth = 51;
+            this.ChapNhanDGV.RowTemplate.Height = 24;
+            this.ChapNhanDGV.Size = new System.Drawing.Size(298, 141);
+            this.ChapNhanDGV.TabIndex = 3;
+            this.ChapNhanDGV.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // TuChoiDGV
+            // 
+            this.TuChoiDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TuChoiDGV.Location = new System.Drawing.Point(475, 316);
+            this.TuChoiDGV.Name = "TuChoiDGV";
+            this.TuChoiDGV.RowHeadersWidth = 51;
+            this.TuChoiDGV.RowTemplate.Height = 24;
+            this.TuChoiDGV.Size = new System.Drawing.Size(298, 140);
+            this.TuChoiDGV.TabIndex = 4;
+            // 
+            // lbChapNhan
+            // 
+            this.lbChapNhan.AutoSize = true;
+            this.lbChapNhan.Location = new System.Drawing.Point(469, 118);
+            this.lbChapNhan.Name = "lbChapNhan";
+            this.lbChapNhan.Size = new System.Drawing.Size(158, 16);
+            this.lbChapNhan.TabIndex = 5;
+            this.lbChapNhan.Text = "Ứng viên được chấp nhận";
+            // 
+            // lbTuChoi
+            // 
+            this.lbTuChoi.AutoSize = true;
+            this.lbTuChoi.Location = new System.Drawing.Point(472, 297);
+            this.lbTuChoi.Name = "lbTuChoi";
+            this.lbTuChoi.Size = new System.Drawing.Size(115, 16);
+            this.lbTuChoi.TabIndex = 6;
+            this.lbTuChoi.Text = "Ứng viên bị từ chối";
+            // 
+            // lbSoLuong
+            // 
+            this.lbSoLuong.AutoSize = true;
+            this.lbSoLuong.Location = new System.Drawing.Point(630, 79);
+            this.lbSoLuong.Name = "lbSoLuong";
+            this.lbSoLuong.Size = new System.Drawing.Size(95, 16);
+            this.lbSoLuong.TabIndex = 7;
+            this.lbSoLuong.Text = "Số lượng tuyển";
+            // 
+            // lbSoLuongYC
+            // 
+            this.lbSoLuongYC.AutoSize = true;
+            this.lbSoLuongYC.Location = new System.Drawing.Point(316, 440);
+            this.lbSoLuongYC.Name = "lbSoLuongYC";
+            this.lbSoLuongYC.Size = new System.Drawing.Size(110, 16);
+            this.lbSoLuongYC.TabIndex = 8;
+            this.lbSoLuongYC.Text = "Số lượng yêu cầu";
+            // 
+            // lbSoLuongChapNhan
+            // 
+            this.lbSoLuongChapNhan.AutoSize = true;
+            this.lbSoLuongChapNhan.Location = new System.Drawing.Point(690, 268);
+            this.lbSoLuongChapNhan.Name = "lbSoLuongChapNhan";
+            this.lbSoLuongChapNhan.Size = new System.Drawing.Size(60, 16);
+            this.lbSoLuongChapNhan.TabIndex = 9;
+            this.lbSoLuongChapNhan.Text = "Số lượng";
+            // 
+            // lbSoLuongTuChoi
+            // 
+            this.lbSoLuongTuChoi.AutoSize = true;
+            this.lbSoLuongTuChoi.Location = new System.Drawing.Point(690, 440);
+            this.lbSoLuongTuChoi.Name = "lbSoLuongTuChoi";
+            this.lbSoLuongTuChoi.Size = new System.Drawing.Size(60, 16);
+            this.lbSoLuongTuChoi.TabIndex = 10;
+            this.lbSoLuongTuChoi.Text = "Số lượng";
+            // 
+            // MhChonNhaTuyenDung
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 504);
+            this.Controls.Add(this.lbSoLuongTuChoi);
+            this.Controls.Add(this.lbSoLuongChapNhan);
+            this.Controls.Add(this.lbSoLuongYC);
+            this.Controls.Add(this.lbSoLuong);
+            this.Controls.Add(this.lbTuChoi);
+            this.Controls.Add(this.lbChapNhan);
+            this.Controls.Add(this.TuChoiDGV);
+            this.Controls.Add(this.ChapNhanDGV);
+            this.Controls.Add(this.lbTenTuyenDung);
+            this.Controls.Add(this.MaHSDGV);
+            this.Controls.Add(this.label1);
+            this.Name = "MhChonNhaTuyenDung";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.MaHSDGV)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ChapNhanDGV)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TuChoiDGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView MaHSDGV;
+        private System.Windows.Forms.Label lbTenTuyenDung;
+        private System.Windows.Forms.DataGridView ChapNhanDGV;
+        private System.Windows.Forms.DataGridView TuChoiDGV;
+        private System.Windows.Forms.Label lbChapNhan;
+        private System.Windows.Forms.Label lbTuChoi;
+        private System.Windows.Forms.Label lbSoLuong;
+        private System.Windows.Forms.Label lbSoLuongYC;
+        private System.Windows.Forms.Label lbSoLuongChapNhan;
+        private System.Windows.Forms.Label lbSoLuongTuChoi;
+    }
+}
+
