e4e35b0ce8b720db44cd3034fbeda9680e732621
Bui Nguyen Tin <buinguyentin28062003@gmail.com>
2024-05-06 15:26:43 +0700

    Create DoanhNghiep.cs

---
+++ PTTK/BUS/DoanhNghiep.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace PTTK.BUS
+{
+    public class DoanhNghiep
+    {
+        public string TenCongty { get; set; }
+        public string MaSoThue { get; set; }
+        public string NguoiDaiDien { get; set; }
+        public string Email { get; set; }
+        public string DiaChi { get; set; }
+        public string MaUD { get; set; }
+
+        public DoanhNghiep LayTTDoanhNghiep { get; set; }
+    }
+
+}
