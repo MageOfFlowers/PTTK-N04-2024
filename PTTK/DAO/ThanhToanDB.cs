9ed5f5abb99ae29fe293e56a44aef5971ecaf283
T4loN2110 <anhtainguyenduc@gmail.com>
2024-05-07 16:34:53 +0700

    .

--- PTTK/DAO/ThanhToanDB.cs
+++ PTTK/DAO/ThanhToanDB.cs
@@ -10,7 +10,7 @@ namespace PTTK.DAO
 {
     internal class ThanhToanDB
     {
-        private readonly SqlConnection connection = new SqlConnection(Program.connString)
+        private readonly SqlConnection connection = new SqlConnection(Program.connString);
         public void ThanhToan(ThanhToan tt)
         {
 
@@ -20,7 +20,11 @@ namespace PTTK.DAO
                 using (SqlCommand command = new SqlCommand(proc_name, connection))
                 {
                     connection.Open();
-                    command.Parameters.Add("@ma_qc", tt);
+                    command.Parameters.AddWithValue("@ma_qc", tt.MaQC);
+                    command.Parameters.AddWithValue("@lan_tra", tt.LanTT);
+                    command.Parameters.AddWithValue("@@so_tien", tt.SoTien);
+                    command.Parameters.AddWithValue("@@ngay_thanh_toan", tt.NgayTT);
+                    command.Parameters.AddWithValue("@@phuong_thuc", tt.PhuongThuc);
                 }
             }
             catch
