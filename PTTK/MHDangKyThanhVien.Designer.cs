d97cebc1790bb420e32490d093d9922348b3fb2e
lehoanglong267 <lehoanglong0267@gmail.com>
2024-05-11 16:28:40 +0700

    Create MHDangKyThanhVien.Designer.cs

---
+++ PTTK/MHDangKyThanhVien.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace PTTK
+{
+	partial class MHDangKyThanhVien
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.TenCongTy = new System.Windows.Forms.Label();
+			this.MaSoThue = new System.Windows.Forms.Label();
+			this.NguoiDaiDien = new System.Windows.Forms.Label();
+			this.Email = new System.Windows.Forms.Label();
+			this.DiaChi = new System.Windows.Forms.Label();
+			this.txtTenCongTy = new System.Windows.Forms.TextBox();
+			this.txtMaSoThue = new System.Windows.Forms.TextBox();
+			this.txtNguoiDaiDien = new System.Windows.Forms.TextBox();
+			this.txtEmail = new System.Windows.Forms.TextBox();
+			this.txtDiaChi = new System.Windows.Forms.TextBox();
+			this.btnDangKy = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.btnQuayLai = new System.Windows.Forms.Button();
+			this.ssDangKy = new System.Windows.Forms.StatusStrip();
+			this.statusDangKy = new System.Windows.Forms.ToolStripStatusLabel();
+			this.ssDangKy.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// TenCongTy
+			// 
+			this.TenCongTy.AutoSize = true;
+			this.TenCongTy.Location = new System.Drawing.Point(74, 127);
+			this.TenCongTy.Name = "TenCongTy";
+			this.TenCongTy.Size = new System.Drawing.Size(85, 16);
+			this.TenCongTy.TabIndex = 0;
+			this.TenCongTy.Text = "Tên Công Ty";
+			// 
+			// MaSoThue
+			// 
+			this.MaSoThue.AutoSize = true;
+			this.MaSoThue.Location = new System.Drawing.Point(74, 87);
+			this.MaSoThue.Name = "MaSoThue";
+			this.MaSoThue.Size = new System.Drawing.Size(80, 16);
+			this.MaSoThue.TabIndex = 1;
+			this.MaSoThue.Text = "Mã Số Thuế";
+			// 
+			// NguoiDaiDien
+			// 
+			this.NguoiDaiDien.AutoSize = true;
+			this.NguoiDaiDien.Location = new System.Drawing.Point(74, 242);
+			this.NguoiDaiDien.Name = "NguoiDaiDien";
+			this.NguoiDaiDien.Size = new System.Drawing.Size(97, 16);
+			this.NguoiDaiDien.TabIndex = 2;
+			this.NguoiDaiDien.Text = "Người Đại Diện";
+			// 
+			// Email
+			// 
+			this.Email.AutoSize = true;
+			this.Email.Location = new System.Drawing.Point(74, 203);
+			this.Email.Name = "Email";
+			this.Email.Size = new System.Drawing.Size(41, 16);
+			this.Email.TabIndex = 3;
+			this.Email.Text = "Email";
+			// 
+			// DiaChi
+			// 
+			this.DiaChi.AutoSize = true;
+			this.DiaChi.Location = new System.Drawing.Point(74, 165);
+			this.DiaChi.Name = "DiaChi";
+			this.DiaChi.Size = new System.Drawing.Size(49, 16);
+			this.DiaChi.TabIndex = 4;
+			this.DiaChi.Text = "Địa Chỉ";
+			// 
+			// txtTenCongTy
+			// 
+			this.txtTenCongTy.Location = new System.Drawing.Point(175, 124);
+			this.txtTenCongTy.Name = "txtTenCongTy";
+			this.txtTenCongTy.Size = new System.Drawing.Size(275, 22);
+			this.txtTenCongTy.TabIndex = 5;
+			// 
+			// txtMaSoThue
+			// 
+			this.txtMaSoThue.Location = new System.Drawing.Point(175, 84);
+			this.txtMaSoThue.Name = "txtMaSoThue";
+			this.txtMaSoThue.Size = new System.Drawing.Size(275, 22);
+			this.txtMaSoThue.TabIndex = 6;
+			// 
+			// txtNguoiDaiDien
+			// 
+			this.txtNguoiDaiDien.Location = new System.Drawing.Point(175, 239);
+			this.txtNguoiDaiDien.Name = "txtNguoiDaiDien";
+			this.txtNguoiDaiDien.Size = new System.Drawing.Size(275, 22);
+			this.txtNguoiDaiDien.TabIndex = 7;
+			// 
+			// txtEmail
+			// 
+			this.txtEmail.Location = new System.Drawing.Point(175, 200);
+			this.txtEmail.Name = "txtEmail";
+			this.txtEmail.Size = new System.Drawing.Size(275, 22);
+			this.txtEmail.TabIndex = 8;
+			// 
+			// txtDiaChi
+			// 
+			this.txtDiaChi.Location = new System.Drawing.Point(175, 162);
+			this.txtDiaChi.Name = "txtDiaChi";
+			this.txtDiaChi.Size = new System.Drawing.Size(275, 22);
+			this.txtDiaChi.TabIndex = 9;
+			// 
+			// btnDangKy
+			// 
+			this.btnDangKy.Location = new System.Drawing.Point(328, 298);
+			this.btnDangKy.Name = "btnDangKy";
+			this.btnDangKy.Size = new System.Drawing.Size(81, 30);
+			this.btnDangKy.TabIndex = 10;
+			this.btnDangKy.Text = "Đăng Ký";
+			this.btnDangKy.UseVisualStyleBackColor = true;
+			this.btnDangKy.Click += new System.EventHandler(this.btnDangKy_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(71, 34);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(397, 31);
+			this.label1.TabIndex = 11;
+			this.label1.Text = "Nhập thông tin Doanh Nghiệp";
+			// 
+			// btnQuayLai
+			// 
+			this.btnQuayLai.Location = new System.Drawing.Point(127, 298);
+			this.btnQuayLai.Name = "btnQuayLai";
+			this.btnQuayLai.Size = new System.Drawing.Size(80, 30);
+			this.btnQuayLai.TabIndex = 12;
+			this.btnQuayLai.Text = "Quay Lại";
+			this.btnQuayLai.UseVisualStyleBackColor = true;
+			this.btnQuayLai.Click += new System.EventHandler(this.btnQuayLai_Click);
+			// 
+			// ssDangKy
+			// 
+			this.ssDangKy.ImageScalingSize = new System.Drawing.Size(20, 20);
+			this.ssDangKy.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusDangKy});
+			this.ssDangKy.Location = new System.Drawing.Point(0, 377);
+			this.ssDangKy.Name = "ssDangKy";
+			this.ssDangKy.Size = new System.Drawing.Size(535, 26);
+			this.ssDangKy.TabIndex = 13;
+			this.ssDangKy.Text = "ssDangKy";
+			// 
+			// statusDangKy
+			// 
+			this.statusDangKy.Name = "statusDangKy";
+			this.statusDangKy.Size = new System.Drawing.Size(98, 20);
+			this.statusDangKy.Text = "statusDangKy";
+			// 
+			// MHDangKyThanhVien
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(535, 403);
+			this.Controls.Add(this.ssDangKy);
+			this.Controls.Add(this.btnQuayLai);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btnDangKy);
+			this.Controls.Add(this.txtDiaChi);
+			this.Controls.Add(this.txtEmail);
+			this.Controls.Add(this.txtNguoiDaiDien);
+			this.Controls.Add(this.txtMaSoThue);
+			this.Controls.Add(this.txtTenCongTy);
+			this.Controls.Add(this.DiaChi);
+			this.Controls.Add(this.Email);
+			this.Controls.Add(this.NguoiDaiDien);
+			this.Controls.Add(this.MaSoThue);
+			this.Controls.Add(this.TenCongTy);
+			this.Name = "MHDangKyThanhVien";
+			this.Text = "Đăng ký thành viên doanh nghiệp";
+			this.ssDangKy.ResumeLayout(false);
+			this.ssDangKy.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label TenCongTy;
+		private System.Windows.Forms.Label MaSoThue;
+		private System.Windows.Forms.Label NguoiDaiDien;
+		private System.Windows.Forms.Label Email;
+		private System.Windows.Forms.Label DiaChi;
+		private System.Windows.Forms.TextBox txtTenCongTy;
+		private System.Windows.Forms.TextBox txtMaSoThue;
+		private System.Windows.Forms.TextBox txtNguoiDaiDien;
+		private System.Windows.Forms.TextBox txtEmail;
+		private System.Windows.Forms.TextBox txtDiaChi;
+		private System.Windows.Forms.Button btnDangKy;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button btnQuayLai;
+		private System.Windows.Forms.StatusStrip ssDangKy;
+		private System.Windows.Forms.ToolStripStatusLabel statusDangKy;
+	}
+}
