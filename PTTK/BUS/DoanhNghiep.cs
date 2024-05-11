d8d8ee43030e3f9c40a4f576bfe58f870ab6b7aa
lehoanglong267 <lehoanglong0267@gmail.com>
2024-05-11 16:29:23 +0700

    Update DoanhNghiep.cs

--- PTTK/BUS/DoanhNghiep.cs
+++ PTTK/BUS/DoanhNghiep.cs
@@ -1,23 +1,40 @@
-﻿using System;
+﻿using PTTK.DAO;
+using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace PTTK.BUS
 {
     public class DoanhNghiep
     {
-        public string TenCongty { get; set; }
-        public string MaSoThue { get; set; }
-        public string NguoiDaiDien { get; set; }
+		public string MaSoThue { get; set; }
+		public string TenCongTy { get; set; }
         public string Email { get; set; }
         public string DiaChi { get; set; }
-        public string MaUD { get; set; }
+		public string NguoiDaiDien { get; set; }
+		public string TrangThai { get; set; }
+		public string UuDai { get; set; }
 
-        public DoanhNghiep LayTTDoanhNghiep { get; set; }
-    }
+		public DoanhNghiep TimThongTinDoanhNghiep(string MaSoThue)
+		{
+			DoanhNghiepDB doanhNghiepDB = new DoanhNghiepDB();
+			return doanhNghiepDB.LayThongTinDoanhNghiep(MaSoThue);
+		}
+		public bool KiemTraTonTai(string MaSoThue)
+		{
+			DoanhNghiepDB doanhNghiepDB = new DoanhNghiepDB();
+			return doanhNghiepDB.KiemTraTonTai(MaSoThue);
+		}
+		public void DangKyThanhVien(DoanhNghiep doanhNghiep)
+		{
+			DoanhNghiepDB doanhNghiepDB = new DoanhNghiepDB();
+			doanhNghiepDB.DangKyThanhVien(doanhNghiep);
+		}
+	}
 
 }
